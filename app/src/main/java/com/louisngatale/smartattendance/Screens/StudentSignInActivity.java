1331d2d2e9808ed9879348baa0fd346c9e3a939a
LouisNgatale <louisngatale@gmail.com>
2021-04-20 16:00:29 +0300

    Scan QR Code

--- app/src/main/java/com/louisngatale/smartattendance/Screens/StudentSignInActivity.java
+++ app/src/main/java/com/louisngatale/smartattendance/Screens/StudentSignInActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
@@ -13,9 +15,14 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
 import com.louisngatale.smartattendance.R;
 
-public class StudentSignInActivity extends AppCompatActivity {
-    private CodeScanner mCodeScanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class StudentSignInActivity extends AppCompatActivity {
+    private static final String TAG = "CREATED";
+    private CodeScanner mCodeScanner;
+    String fullName,id,course;
+    Button proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +30,68 @@ public class StudentSignInActivity extends AppCompatActivity {
 
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+
+        proceed = findViewById(R.id.proceed);
+
+        mCodeScanner.setDecodeCallback(result -> runOnUiThread(new Runnable() {
             @Override
-            public void onDecoded(@NonNull final Result result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(StudentSignInActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+            public void run() {
+//                        String ID_PATTERN = "/time:(.*)ID:/gmi";
+                String ID_PATTERN = "ID:.\\d+";
+//                        String NAME_PATTERN = "ID:.\\d+(.* )verify:";
+                String NAME_PATTERN = "ID:.\\d+(.* )Course:";
+                String COURSE_PATTERN = "Course:.(.*) :";
+
+                fullName = getName(NAME_PATTERN);
+                id = getId(ID_PATTERN);
+                course = getCourse(COURSE_PATTERN);
+                proceed.setEnabled(true);
+
+            }
+
+            private String getId(String ID_PATTERN) {
+                Pattern pattern = Pattern.compile(ID_PATTERN, Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(result.getText());
+                if (matcher.find()) {
+                    String group = matcher.group();
+                    Pattern pattern1 = Pattern.compile("\\d+", Pattern.CASE_INSENSITIVE);
+                    Matcher matcher2 = pattern1.matcher(group);
+                    if (matcher2.find()) {
+                        return matcher2.group();
                     }
-                });
+                    return null;
+                }
+                return null;
             }
-        });
-        scannerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
+
+            private String getName(String NAME_PATTERN) {
+                Pattern pattern = Pattern.compile(NAME_PATTERN, Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(result.getText());
+                if (matcher.find()) {
+                    String studentName = matcher.group(1);
+                    String substring = studentName.substring(studentName.indexOf(",") + 2);
+                    String substring2 = studentName.substring(0,studentName.indexOf(","));
+                    return substring.concat(substring2);
+                }
+                return null;
             }
+
+            private String getCourse(String COURSE_PATTERN) {
+                Pattern pattern = Pattern.compile(COURSE_PATTERN, Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(result.getText());
+                if (matcher.find()) {
+                    String group = matcher.group();
+                    return group.substring(group.indexOf(":") + 1, group.lastIndexOf(":") - 1);
+                }
+                return null;
+            }
+        }));
+        
+        scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
+
+
+        proceed.setOnClickListener(v -> {
+            Log.d(TAG, "onCreate: " + fullName + " " + id + " " + course);
         });
 
     }
