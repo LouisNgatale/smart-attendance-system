728f7ba13dfa726b43b7e8c113279965fdcd7184
LouisNgatale <louisngatale@gmail.com>
2021-04-20 12:37:03 +0300

    Welcome page update& qr scanner setup

---
+++ app/src/main/java/com/louisngatale/smartattendance/Screens/StudentSignInActivity.java
@@ -0,0 +1,57 @@
+package com.louisngatale.smartattendance.Screens;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.louisngatale.smartattendance.R;
+
+public class StudentSignInActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_sign_in);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(StudentSignInActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
--- app/src/main/java/com/louisngatale/smartattendance/Screens/WelcomeActivity.java
+++ app/src/main/java/com/louisngatale/smartattendance/Screens/WelcomeActivity.java
@@ -2,15 +2,26 @@ package com.louisngatale.smartattendance.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.louisngatale.smartattendance.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Button signIn,Register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener((View v) -> {
+            Intent signInIntent = new Intent(WelcomeActivity.this,StudentSignInActivity.class);
+            startActivity(signInIntent);
+        });
     }
 }
