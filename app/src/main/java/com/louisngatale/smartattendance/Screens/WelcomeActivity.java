89356076e1757b61765f7531eb2ae5cce44a0739
LouisNgatale <louisngatale@gmail.com>
2021-04-20 11:37:14 +0300

    first commit

---
+++ app/src/main/java/com/louisngatale/smartattendance/Screens/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.louisngatale.smartattendance.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.louisngatale.smartattendance.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
