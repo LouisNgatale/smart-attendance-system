0c1de3cb76947f764eb6fe9205987365b5f99929
Louis Ngatale <louisngatale@gmail.com>
2021-07-02 21:31:30 +0300

    Teacher gen code back button fix

--- app/src/main/java/com/louisngatale/smartattendance/Screens/ProtectedRoutes/Teacher/QrCodeGenerator.java
+++ app/src/main/java/com/louisngatale/smartattendance/Screens/ProtectedRoutes/Teacher/QrCodeGenerator.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.database.collection.LLRBNode;
@@ -21,12 +22,14 @@ import androidmads.library.qrgenearator.QRGEncoder;
 
 public class QrCodeGenerator extends AppCompatActivity {
     ImageView qrImage;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code_generator);
         qrImage = findViewById(R.id.qrPlaceHolder);
+        back = findViewById(R.id.back);
 
         Intent intent = getIntent();
 
@@ -38,6 +41,10 @@ public class QrCodeGenerator extends AppCompatActivity {
             Bitmap qrBits = qrgEncoder.getBitmap();
             qrImage.setImageBitmap(qrBits);
         }
+
+        back.setOnClickListener(v -> {
+            finish();
+        });
 
     }
 }
